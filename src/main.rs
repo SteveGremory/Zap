046651009cab706ca0588cda5bc62314efbcb083
Steve Gremory <38187217+SteveGremory@users.noreply.github.com>
2022-10-22 17:07:21 +0530

    Added compression, arg-parse.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#![feature(seek_stream_len)]
 mod encryption;
+use argparse::{ArgumentParser, Store};
 
+use lz4::block::{compress, decompress};
 use serde::{Deserialize, Serialize};
 use std::{
-    env,
     fs::File,
     io::{Read, Write},
     path::{Path, PathBuf},
@@ -37,7 +37,7 @@ impl FileData {
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Container(Vec<FileData>);
 
-fn create_combined_file(folder_path: &String) {
+fn create_combined_file(folder_path: &String, file_path: &String) {
     // A container for all the files that have been read
     // AKA the big blob of data
     let mut container_vec: Vec<FileData> = Vec::new();
@@ -58,11 +58,14 @@ fn create_combined_file(folder_path: &String) {
             .read_to_end(&mut file_data)
             .expect("Failed to read the specified file.");
 
+        // Compress the file data
+        let compressed_data = compress(&file_data, None, true).unwrap();
+
         // Construct a new FileData struct
         let file: FileData = FileData::new(
             entry_path.strip_prefix(folder_path).unwrap().to_path_buf(),
             file_size,
-            file_data,
+            compressed_data,
         );
         container_vec.push(file);
     }
@@ -73,13 +76,13 @@ fn create_combined_file(folder_path: &String) {
     let encoded_metadata = bincode::serialize(&container).unwrap();
 
     // write it to disk.
-    let mut combined_file = File::create("/tmp/testing").unwrap();
+    let mut combined_file = File::create(file_path).unwrap();
     combined_file.write_all(&encoded_metadata).unwrap();
 }
 
-fn read_combined_file() -> Vec<FileData> {
+fn read_combined_file(file_path: String) -> Vec<FileData> {
     // Read the encoded data from the disk
-    let mut container_fp = File::open("/tmp/testing").unwrap();
+    let mut container_fp = File::open(file_path).unwrap();
     let mut container_data = Vec::new();
     container_fp.read_to_end(&mut container_data).unwrap();
 
@@ -97,9 +100,12 @@ async fn recreate_files(combined_data: Vec<FileData>) {
 
         let mut file_write = tokio::fs::File::create(filepath).await.unwrap();
 
+        // Decompress the data
+        let decompressed_data = decompress(&i.data, None).unwrap();
+
         let write_task = task::spawn(async move {
             file_write
-                .write_all(&i.data)
+                .write_all(&decompressed_data)
                 .await
                 .expect("Failed to write to new temp file.")
         });
@@ -113,11 +119,57 @@ async fn recreate_files(combined_data: Vec<FileData>) {
 
 #[tokio::main]
 async fn main() {
-    shred_file("/tmp/testing");
-    let args: Vec<String> = env::args().collect();
+    let mut folder_path = String::new();
+    let mut output_path = String::new();
+    let mut file_path = String::new();
 
-    create_combined_file(&args[1]);
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("SecureFolder V0.1: Encrypt and package a given folder into one file.");
 
-    let combined_data = read_combined_file();
-    recreate_files(combined_data).await;
+        ap.refer(&mut folder_path).add_option(
+            &["--folderpath"],
+            Store,
+            "Path to the folder to be encrypted.",
+        );
+
+        ap.refer(&mut file_path).add_option(
+            &["--filepath"],
+            Store,
+            "Path to the combined file to be accessed; the contents will be placed in the current directory.",
+        );
+        ap.refer(&mut output_path)
+            .add_option(
+                &["-o", "--output"],
+                Store,
+                "Path to the output file; Beware if a file exists with the same name, that the file will be shredded.",
+            );
+
+        ap.parse_args_or_exit();
+    }
+
+    // If an output file isn't found, then tell the user to create one
+    // as rust is facing issues creating files on macOS.
+    if !output_path.is_empty() {
+        if !Path::new(&output_path).exists() {
+            panic!(
+                "File {} was not found. Please create it before proceeding.",
+                output_path
+            );
+        } else {
+            // Shred the previous file with that name
+            shred_file(&output_path);
+        }
+    }
+
+    // If a combined file is to be created, do so
+    // if not, then recreate the file strucutre from the combined file.
+    if file_path.is_empty() {
+        // Create a combined file from the folder
+        create_combined_file(&folder_path, &output_path);
+    } else {
+        // Recreate the file structure that was combined.
+        let combined_data = read_combined_file(file_path);
+        recreate_files(combined_data).await;
+    }
 }
