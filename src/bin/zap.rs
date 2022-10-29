56aebaa5fc6dee501c3f496431411ed4439cadf6
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-10-29 23:09:35 +0200

    `zap` and `unzap` are now a single binary: `zap`
    Both archiving and extraction are available from a single binary, each
    triggered by their own subcommand.

--- src/bin/zap.rs
+++ src/bin/zap.rs
@@ -3,43 +3,90 @@ use std::{
     io::{self, BufWriter},
 };
 
-use clap::Parser;
-use zapf::pack_files;
+use clap::{Parser, Subcommand};
+use zapf::{pack_files, unpack_files};
 
-#[derive(Parser, Debug)]
+#[derive(Debug, Parser)]
 #[command(
     author,
     version,
     about = "Zap is a simple program to compress/encrypt the a folder."
 )]
 struct Args {
-    /// Input folder
-    input: String,
+    #[command(subcommand)]
+    command: Command,
+}
 
-    /// Output file
-    output: String,
+#[derive(Debug, Subcommand)]
+enum Command {
+    Archive {
+        /// Input folder
+        input: String,
+        /// Output file
+        output: String,
 
-    /// Whether to encrypt the data
-    #[arg(short, long)]
-    encrypt: bool,
+        /// Whether to encrypt the data
+        #[arg(short, long)]
+        encrypt: bool,
+    },
+    Extract {
+        /// Input file
+        input: String,
+
+        /// Output folder
+        output: String,
+
+        /// Whether to encrypt the data
+        #[arg(short, long)]
+        decrypt: bool,
+    },
+}
+
+impl Command {
+    async fn execute(self) -> io::Result<()> {
+        match self {
+            Command::Archive {
+                input,
+                output,
+                encrypt,
+            } => Self::archive(input, output, encrypt).await,
+            Command::Extract {
+                input,
+                output,
+                decrypt,
+            } => Self::extract(input, output, decrypt).await,
+        }
+    }
+
+    async fn archive(input: String, output: String, encrypt: bool) -> io::Result<()> {
+        if encrypt {
+            todo!("Encryption has not been implemented yet.");
+        }
+
+        zap::compress_directory(&input, "/tmp/stuff").await?;
+
+        let out_file = File::create(&output).expect("Could not create file");
+        let mut out_writer = BufWriter::new(out_file);
+
+        pack_files("/tmp/stuff", &mut out_writer)?;
+
+        fs::remove_dir_all("/tmp/stuff")
+    }
+
+    async fn extract(input: String, output: String, decrypt: bool) -> io::Result<()> {
+        if decrypt {
+            todo!("Decryption has not been implemented yet.");
+        }
+
+        unpack_files(&input, "/tmp/unpacked")?;
+        zap::decompress_directory("/tmp/unpacked", &output).await?;
+
+        fs::remove_dir_all("/tmp/unpacked")
+    }
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let args = Args::parse();
-
-    if args.encrypt {
-        todo!("Encryption has not been implemented yet.");
-    }
-
-    zap::compress_directory(&args.input, "/tmp/stuff").await?;
-
-    let out_file = File::create(&args.output).expect("Could not create file");
-    let mut out_writer = BufWriter::new(out_file);
-
-    pack_files("/tmp/stuff", &mut out_writer)?;
-
-    fs::remove_dir_all("/tmp/stuff")?;
-
-    Ok(())
+    args.command.execute().await
 }
